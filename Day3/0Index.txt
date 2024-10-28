7e6c1cab1669fded3516dae2de935f189db247cc
srinija kambhampaty <srinija@apples-MacBook-Pro.local>
2024-10-28 16:58:20 +0530

    Basic function implementation and Sorting algorithms

---
+++ Day3/0Index.txt
@@ -0,0 +1,7 @@
+Array Related Tasks:
+Implement functions that takes array as parameters and does the following to the original array-
+1)Bubble Sort
+2)Insertion Sort
+3) Quick Sort
+4) Heap Sort
+5) Selection Sort
---
+++ Day3/first.scala
@@ -0,0 +1,16 @@
+// object Fisrt{
+//     def main(args: Array[String]): Unit = {
+//         println("helllo world")
+//         println(args)
+//     }
+// } 
+
+object Runit extends App {
+    println("hello through runint")
+    println(args)
+}
+
+// @main def first(name: String, age: Int): Unit = {
+//     println("hello from anotation like python")
+//     println(s"Name: $name, Age: $age")
+// }
