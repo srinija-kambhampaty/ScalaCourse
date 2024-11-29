611958037099b0259fbf3a81e0f19a30f2b8bec3
srinija-kambhampaty <kambhampatysrinija@gmail.com>
2024-11-29 12:15:16 +0530

    Day15

--- Day15/Exercises/src/main/scala/Exercise_4.scala
+++ Day15/Exercises/src/main/scala/Exercise_4.scala
@@ -1,3 +1,25 @@
-class Exercise_4 {
+import org.apache.spark.sql.SparkSession
 
+object Exercise_4 {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder
+      .appName("Exercise_4")
+      .master("local[2]")
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+    val numbers = sc.parallelize(1 to 1000)
+    val evenNumbers = numbers.filter(_ % 2 == 0)
+    val multipliedNumbers = evenNumbers.map(_ * 10)
+    val keyValuePairs = multipliedNumbers.map(num => (num % 100, num))
+    val groupedSums = keyValuePairs.reduceByKey(_ + _)
+    val results = groupedSums.collect()
+    results.foreach { case (key, sum) =>
+      println(s"Remainder $key: Sum = $sum")
+    }
+    Thread.sleep(30000)
+
+    spark.stop()
+
+  }
 }
